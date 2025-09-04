8be6ed3066bf4e83b62a1f7b08f381f848275578
Hector B <hectorbg303@gmail.com>
2025-09-04 19:23:27 +0200

    routes

--- js/services/Router.js
+++ js/services/Router.js
@@ -12,7 +12,47 @@ const Router = {
     Router.go(location.pathname);
   },
   go: (route, addToHistory = true) => {
-    console.log(route);
+    if (addToHistory) {
+      history.pushState({ route }, "", route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("menu-page");
+        break;
+      case "/html":
+        pageElement = document.createElement("question-page");
+        pageElement.dataset.section = "html";
+        pageElement.dataset.questionId = "1";
+        break;
+      case "/css":
+        pageElement = document.createElement("question-page");
+        pageElement.dataset.section = "html";
+        pageElement.dataset.questionId = "1";
+        break;
+      case "/javascript":
+        pageElement = document.createElement("question-page");
+        pageElement.dataset.section = "html";
+        pageElement.dataset.questionId = "1";
+        break;
+      case "/accessibility":
+        pageElement = document.createElement("question-page");
+        pageElement.dataset.section = "html";
+        pageElement.dataset.questionId = "1";
+        break;
+      default:
+        if (route.startsWith("/score-")) {
+          pageElement = document.createElement("score-page");
+          pageElement.dataset.section = route.substring(
+            route.lastIndexOf("-") + 1
+          );
+        }
+        break;
+    }
+    if (pageElement) {
+      document.querySelector("main").innerHTML = "";
+      document.querySelector("main").appendChild(pageElement);
+    }
   },
 };
 
